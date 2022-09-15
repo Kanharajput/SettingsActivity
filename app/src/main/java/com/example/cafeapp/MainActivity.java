b7ddc0be1d77e39d480c97cf280037b94d354480
kanharajput91 <kanharajput91@gmail.com>
2022-09-15 10:38:04 +0530

    Settings Activity added to maintain settings

--- app/src/main/java/com/example/cafeapp/MainActivity.java
+++ app/src/main/java/com/example/cafeapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cafeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -55,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this,SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
